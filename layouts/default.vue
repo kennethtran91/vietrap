faa55cc43a0e30b8f4d636018c3b10d99a5f0f74
Kenneth Tran <kennethtran@Kenneths-MacBook-Pro.local>
2018-06-02 19:19:04 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app dark>
+      <v-toolbar>
+      <v-layout row>
+        <v-flex xs6>
+      <v-text-field 
+      name="searchbox"
+      label="Search songs and artists ..."
+      single-line
+      ></v-text-field></v-flex></v-layout>
+    <v-toolbar-title>viet rap 2018</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Sign up</v-btn>
+      <v-btn flat>Sign in</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+
+    
+  <v-toolbar dark color="primary" dense>
+  <v-layout row justify-center>
+  <v-toolbar-items class="text-xs-center">
+  <div class="text-xs-center">
+      <v-btn flat>Home</v-btn>
+      <v-btn flat>artirts</v-btn>
+      <v-btn flat>Add songs</v-btn>
+      <v-btn flat>All Songs</v-btn>
+      </div>
+    </v-toolbar-items>
+  </v-layout>
+  </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>viet rap</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
